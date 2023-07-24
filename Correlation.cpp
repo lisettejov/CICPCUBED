caa7870083483c74e2963cb540772f661671af33
lisettejov <89407201+lisettejov@users.noreply.github.com>
2023-07-23 23:20:41 -0700

    Update Correlation.cpp

--- Correlation.cpp
+++ Correlation.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include "Mastercard.dat"
+#include "Visa.dat"
+#include <ctime>
+#include <cstring>
+
+// must include: 
+//  value of R is: 0.9694.
+// Postively correlated
+// As x goes up, so does y
 
